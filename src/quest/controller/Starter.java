82a992feac0973d0ce0d9b970b10080e159684fa
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2016-10-07 15:57:23 +0500

    Проверяю систему логгирования.
    Сами логи не должны быть в репозитории, поэтому добавляем папку с логами
    в gitignore

--- src/quest/controller/Starter.java
+++ src/quest/controller/Starter.java
@@ -1,8 +1,13 @@
 package quest.controller;
 
+import static quest.controller.log.Logger.MsgType.ERROR;
+import static quest.controller.log.Logger.MsgType.INFO;
+import static quest.controller.log.Logger.MsgType.WARNING;
+
 import java.io.IOException;
 import java.net.SocketException;
 
+import quest.controller.log.Logger;
 import quest.controller.udp.QuestHttpServer;
 import quest.controller.udp.UDPServer;
 import quest.model.Quest;
@@ -13,6 +18,18 @@ public class Starter {
 	public static UDPServer			udpServer;
 
 	public static void main(String... strings) {
+		Logger log = Logger.inst();
+
+		log.print("Hello, this is info1", INFO);
+		log.print("Hello, this is info2", INFO);
+		log.print("Hello, this is warning1", WARNING);
+		log.print("Hello, this is warning2", WARNING);
+		log.print("Hello, this is error1", ERROR);
+		log.print("Hello, this is error2", ERROR);
+
+	}
+
+	public static void main2(String... strings) {
 		try {
 			httpServer = new QuestHttpServer();
 		} catch (IOException e1) {
