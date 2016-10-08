f91d1034f13efb82b91119771fe64148fcec66b7
Mikhail <mikhail.chernoskutov@gmail.com>
2016-10-08 20:15:23 +0500

    Пара проверочных классов. Mock для UDP.

--- src/quest/controller/QuestStarter.java
+++ src/quest/controller/QuestStarter.java
@@ -3,14 +3,19 @@ package quest.controller;
 import static quest.controller.log.QLog.MsgType.ERROR;
 import static quest.controller.log.QLog.MsgType.INFO;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 
 import quest.controller.log.QLog;
+import quest.controller.net.Addresser;
 import quest.controller.net.tcp.QuestHttpServer;
 import quest.controller.net.udp.McuUdpServer;
 import quest.model.common.classes.MicroUnit;
-import quest.model.quest1.Quest;
+import quest.model.quest1.TestingStructure;
 
 public class QuestStarter {
 
@@ -18,16 +23,20 @@ public class QuestStarter {
 	public static McuUdpServer udpServer;
 
 	public static void main(String... strings) {
+
 		final QLog LOG = QLog.inst();
-		Quest quest = Quest.inst();
-		MicroUnit.getMicrounits(quest).forEach(a -> LOG.print(a.toString(), INFO));
-		System.exit(0);
-		try {
-			httpServer = new QuestHttpServer();
-		} catch (IOException e1) {
-			LOG.print("Не смог запустить веб-сервер. " + e1.getLocalizedMessage(), ERROR);
+		TestingStructure quest = new TestingStructure();
+		System.out.println("Введите IP адрес для контроллера:");
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));) {
+			InetSocketAddress addr = null;
+			while ((addr = Addresser.getSocketAddress(reader.readLine(), 1024)) == null)
+				;
+			quest.rings.setAddress(addr);
+		} catch (IOException e2) {
+			LOG.print(e2.getLocalizedMessage(), ERROR);
 			return;
 		}
+
 		LOG.print("Теперь запустим UDP сервер", INFO);
 		try {
 			udpServer = new McuUdpServer(2016);
@@ -37,8 +46,14 @@ public class QuestStarter {
 			new Thread(udpServer).start();
 		} catch (SocketException e) {
 			LOG.print("Не смог запустить сервер контроллеров", ERROR);
-			return;
 		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		quest.rings.lock(true);
+
 	}
 
 }
